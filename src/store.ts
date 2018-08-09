05527eae9fdedc679fd8055c01f7ceb56874d67d
helloitsjoe <jboyle617@gmail.com>
2018-08-08 22:20:00 -0400

    cleanup

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { auctionItems } from './reducers/auctionItemsReducer';
-import promise from 'redux-promise-middleware';
+// import promise from 'redux-promise-middleware';
 
-const middleware = applyMiddleware(promise());
+// const middleware = applyMiddleware(promise());
 // const rootReducer = combineReducers({ auctionItems: auctionItems });
 
-export const initStore = () => createStore(auctionItems, middleware);
+export const initStore = () => createStore(auctionItems);
