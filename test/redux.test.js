393f0ca94e8ba13905fc374d08568535fd7dc1ca
helloitsjoe <jboyle617@gmail.com>
2018-10-31 22:37:36 -0400

    add redux test stubs

--- test/redux.test.js
+++ test/redux.test.js
@@ -3,7 +3,8 @@ import {
     initStore
 } from "../src/store";
 import {
-    addItem
+    addItem,
+    deleteRequest
 } from "../src/actions/adminActions";
 import {
     createNewAuctionItem
@@ -31,18 +32,59 @@ describe("redux duck tests", () => {
         expect(store.getState()).toEqual(initialState);
     });
 
-    it("add item", function () {
-        expect(store.getState().auctionItems.length).toBe(0);
-        store.dispatch(addItem());
-        expect(store.getState().auctionItems.length).toBe(1);
-        // TODO: Make this id unique (and settable?)
-        expect(store.getState().auctionItems[0].id).toBe(0);
+    describe('Admin', function () {
+
+        it("add item to store", function () {
+            expect(store.getState().auctionItems.length).toBe(0);
+            store.dispatch(addItem());
+            expect(store.getState().auctionItems.length).toBe(1);
+            // TODO: Make this id unique (and settable?) Use .title instead of .id?
+            expect(store.getState().auctionItems[0].id).toBe(0);
+        });
+
+        it('add item no-op if selected has no title/description', function () {
+
+        });
+
+        it('deleteItem removes item from store', function () {
+            // store.dispatch(addItem());
+            // store.dispatch(addItem());
+            // expect(store.getState().auctionItems.length).toBe(2);
+            // TODO: Figure out how best to test this thunk
+            // store.dispatch(deleteRequest(0));
+            // expect(store.getState().auctionItems.length).toBe(1);
+            // expect(store.getState().auctionItems.find(item => item.id === 1)).toBeTruthy();
+            // expect(store.getState().auctionItems.find(item => item.id === 0)).toBeUndefined();
+        });
+
+        it('deleting last item replaces item with blank item', function () {});
+
+        it('save requires title and description', function () {});
+
+        it('select item', function () {});
     });
 
-    it('toggle item description', function () {
-        store.dispatch(addItem());
-        expect(store.getState().auctionItems[0].viewDetails).toBe(false);
-        store.dispatch(toggleDescriptionAction(0));
-        expect(store.getState().auctionItems[0].viewDetails).toBe(true);
+    describe('User', function () {
+
+        it('load auction data', function () {});
+
+        it('error getting auction data', function () {});
+
+        it('show/hide details', function () {
+            store.dispatch(addItem());
+            expect(store.getState().auctionItems[0].viewDetails).toBe(false);
+            store.dispatch(toggleDescriptionAction(0));
+            expect(store.getState().auctionItems[0].viewDetails).toBe(true);
+            store.dispatch(toggleDescriptionAction(0));
+            expect(store.getState().auctionItems[0].viewDetails).toBe(false);
+        });
+
+        it('quick bid increments bid', function () {});
+
+        it('title input change', function () {});
+
+        it('description input change', function () {});
+
+        it('minimum bid input change', function () {});
     });
 });
