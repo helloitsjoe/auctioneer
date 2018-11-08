1b0e3609d3b00164c7b184227ef1accb3cd5e85b
helloitsjoe <jboyle617@gmail.com>
2018-11-07 22:01:37 -0500

    Poller tests

---
+++ test/Poller.test.js
@@ -0,0 +1,22 @@
+import { Poller } from '../src/Poller';
+
+describe('Poller', function () {
+
+    it('calls function during setInterval', function () {
+        jest.useFakeTimers();
+        const poller = new Poller();
+        const func = jest.fn();
+        expect(poller.isPolling).toBe(false);
+        poller.init(func);
+        expect(poller.isPolling).toBe(true);
+        jest.advanceTimersByTime(1000);
+        expect(setInterval).toBeCalledTimes(1);
+        expect(func).toBeCalledTimes(2);
+        poller.stop();
+        expect(poller.isPolling).toBe(false);
+        expect(clearInterval).toBeCalled();
+        poller.start();
+        expect(poller.isPolling).toBe(true);
+        poller.stop();
+    });
+});
