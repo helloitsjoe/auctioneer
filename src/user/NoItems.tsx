606f189866cc6f4154ba6aa68ce3096b351b6fbb
helloitsjoe <jboyle617@gmail.com>
2019-02-08 00:13:18 -0500

    rename EmptyList to NoItems

---
+++ src/user/NoItems.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function NoItems() {
+    return (
+        <div className="item-group clearfix">
+            <div className="item-container">
+                <span>No bids yet!</span>
+            </div>
+        </div>
+    );
+}
