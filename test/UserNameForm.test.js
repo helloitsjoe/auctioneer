6316a69b0bc13a077eb24ce5824fbc6010e647cb
helloitsjoe <jboyle617@gmail.com>
2019-09-30 19:21:44 -0400

    Update UserNameForm

--- test/UserNameForm.test.js
+++ test/UserNameForm.test.js
@@ -3,42 +3,42 @@ import { mount } from 'enzyme';
 import { UserNameForm } from '../src/user/UserNameForm';
 
 describe('UserNameForm', function() {
-    let userNameForm;
-    let input;
+  let userNameForm;
+  let input;
 
-    beforeEach(() => {
-        userNameForm = mount(<UserNameForm user="Joe" />);
-        input = userNameForm.find('input');
-    });
+  beforeEach(() => {
+    userNameForm = mount(<UserNameForm initialUserName="Joe" />);
+    input = userNameForm.find('input');
+  });
 
-    afterEach(() => {
-        userNameForm.unmount();
-    });
+  afterEach(() => {
+    userNameForm.unmount();
+  });
 
-    it('displays uppercase username', function() {
-        expect(input.html()).toMatch(/JOE/);
-    });
+  it('displays uppercase username', function() {
+    expect(input.html()).toMatch(/JOE/);
+  });
 
-    it('update username on change', function() {
-        input.prop('onChange')({ target: { value: 'Missy' } });
-        expect(input.html()).toMatch(/MISSY/);
-        expect(input.html()).not.toMatch(/JOE/);
-    });
+  it('update username on change', function() {
+    input.prop('onChange')({ target: { value: 'Missy' } });
+    expect(input.html()).toMatch(/MISSY/);
+    expect(input.html()).not.toMatch(/JOE/);
+  });
 
-    it('blur input on submit', function() {
-        const select = jest.fn();
-        const preventDefault = jest.fn();
-        expect(document.activeElement.tagName).toBe('BODY');
-        input.prop('onFocus')({ target: { select } });
-        expect(document.activeElement.tagName).toBe('INPUT');
-        userNameForm.find('form').prop('onSubmit')({ preventDefault });
-        expect(preventDefault).toBeCalledTimes(1);
-        expect(document.activeElement.tagName).toBe('BODY');
-    });
+  it('blur input on submit', function() {
+    const select = jest.fn();
+    const preventDefault = jest.fn();
+    expect(document.activeElement.tagName).toBe('BODY');
+    input.prop('onFocus')({ target: { select } });
+    expect(document.activeElement.tagName).toBe('INPUT');
+    userNameForm.find('form').prop('onSubmit')({ preventDefault });
+    expect(preventDefault).toBeCalledTimes(1);
+    expect(document.activeElement.tagName).toBe('BODY');
+  });
 
-    it('select all text on focus', function() {
-        const select = jest.fn();
-        input.prop('onFocus')({ target: { select } });
-        expect(select).toBeCalledTimes(1);
-    });
+  it('select all text on focus', function() {
+    const select = jest.fn();
+    input.prop('onFocus')({ target: { select } });
+    expect(select).toBeCalledTimes(1);
+  });
 });
